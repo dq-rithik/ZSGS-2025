25e01334aac7981adce6dcb1004d1a02fadd5fd6
Karthik <119005326+dq-rithik@users.noreply.github.com>
2025-07-21 09:45:16 +0530

    Update PatternProgram.java

--- Assignment 2/Part 1/PatternProgram.java
+++ Assignment 2/Part 1/PatternProgram.java
@@ -1,55 +1,6 @@
 public class PatternProgram {
 
-   
-    static int factorial(int n) {
-        int fact = 1;
-        int i = 1;
-        do {
-            fact *= i;
-            i++;
-        } while (i <= n);
-        return fact;
-    }
-
-   
-    static void printPascalsTriangle(int rows) {
-        int i = 0;
-        do {
-            int space = 1;
-            do {
-                if (space <= rows - i)
-                    System.out.print(" ");
-                space++;
-            } while (space <= rows);
-
-            int j = 0;
-            do {
-                int value = factorial(i) / (factorial(j) * factorial(i - j));
-                System.out.print(value + " ");
-                j++;
-            } while (j <= i);
-
-            System.out.println();
-            i++;
-        } while (i < rows);
-    }
-
-    
-    static void printZohoPattern() {
-        String input = "ZOHO CORP ORAT IONS";
-        String[] words = input.split(" ");
-        int i = 0;
-        do {
-            System.out.println(words[i]);
-            i++;
-        } while (i < words.length);
-    }
-
     public static void main(String[] args) {
-        System.out.println("a) Pascal Triangle:");
-        printPascalsTriangle(5);
-
-        System.out.println("ZOHO Pattern:");
-        printZohoPattern();
+      
     }
 }
